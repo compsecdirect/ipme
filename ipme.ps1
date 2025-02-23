8b1cda0210d02d4a59738cfd33cc74f0e4f5eb60
compsecdirect <github@compsecdirect.com>
2025-02-23 10:41:22 -0500

    Update ipme.ps1
    Improvements and 2025 updates and checks.

--- ipme.ps1
+++ ipme.ps1
@@ -4,41 +4,61 @@
             Based on: PaulSec Send-Mail
             Author  :  compsecdirect.com
            
-            Date    : 28-Nov-2018 
+            Date    : 23-Feb-2025 
             File    : ipme.ps1 
             Purpose : Get Static Ip adress of the Internet, email when different
              
-            Version : 2 
+            Version : 3
 #> 
+    # purpose: check if the public ip for a host has changed. When it does, you get an email with the new ip, and the old ip is replaced.
 
-    $url = "https://whatismyip.akamai.com/"
-    $HTML = Invoke-WebRequest -Uri $URl
-    $IP= $html.Content
-    $IP | Out-File "C:\Users\name\Documents\ip.txt"
-    $OldIP = Get-Content "C:\Users\name\Documents\ip-old.txt" 
+    # setup; deploy on windows host and add to scheduled tasks. Set time between 1 day or 1 hour.
+
+    # vars
+
     $smtpServer = "smtp.gmail.com" 
+    $ip_file = "C:\Users\user\Documents\ip.txt"
+    $old_ip_file ="C:\Users\user\Documents\ip-old.txt"
 
     Function mail_away {
 
-    $email = "source@gmail.com" 
-    $email2 = "dest@gmail.com" 
-    $pass = "pass" 
+    $source_email = "sender@gmail.com" 
+    $dest_email = "dest@gmail.com" 
+    $pass = "" 
     $SMTPServer = 'smtp.gmail.com'
     $SMTPPort = '587'
-    $subject = 'IP Change Reported'
+    $subject = "IP Change Reported: $env:computername"
     $data = "IP is now: $IP"
  
   
     $smtp = New-Object System.Net.Mail.SmtpClient($SMTPServer, $SMTPPort);
     $smtp.EnableSSL = $true
-    $smtp.Credentials = New-Object System.Net.NetworkCredential($email, $pass);
-    $smtp.Send($email, $email2, $subject, $data);
+    $smtp.Credentials = New-Object System.Net.NetworkCredential($source_email, $pass);
+    $smtp.Send($source_email, $dest_email, $subject, $data);
        
-    } 
+    }
+    
+    # Tested Feb 22, 2025
+    $url = "https://whatismyip.akamai.com/"
 
+    $HTML = Invoke-WebRequest -Uri $URl
+    $IP= $html.Content
+    $IP | Out-File $ip_file
+    if (Test-Path $old_ip_file -PathType Leaf)
+    {
+        $OldIP = Get-Content $old_ip_file 
+    }
+
+    else
+    {
+        Write-Host "Looks like this is the first time running this script make sure the IP saved is correct."
+        $IP | Out-File $old_ip_file
+        exit
+    }
+    
     if ($Ip -ne $OldIP) {
     mail_away
-    $IP | Out-File "C:\Users\name\Documents\ip-old.txt"
+    $IP | Out-File $old_ip_file
     }
 
     ### end of the script..... 
